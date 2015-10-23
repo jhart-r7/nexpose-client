edeb9d0358257e124a099276b96926f5aeec207b
Jon Hart <jon_hart@rapid7.com>
2015-10-22 19:19:11 -0700

    Basic site add/delete/scan

--- lib/nexpose/cli/site.rb
+++ lib/nexpose/cli/site.rb
@@ -1,16 +1,63 @@
 # encoding: utf-8
 
+require 'securerandom'
 require 'thor'
 
 module Nexpose
   class SiteCLI < Thor
+
+    desc 'add', 'Add a site'
+    option :name
+    def add(*assets)
+      name = options[:name] || SecureRandom.hex
+      $connections.map do |connection|
+        site = Site.new(name)
+        assets.map do |asset|
+          site.include_asset(asset)
+        end
+        site.save(connection)
+        puts "Created #{name} on #{connection.host}"
+      end
+    end
+
+    desc 'delete', 'Delete sites'
+    option :all, :aliases => :a, :type => :boolean
+
+    def delete(*site_names)
+      $connections.map do |connection|
+        if options[:all]
+          connection.sites.map do |site_summary|
+            puts "Deleting #{site_summary.name}/#{site_summary.id} on #{connection.host}"
+            connection.delete_site(site_summary.id)
+          end
+        else
+          site_names.each do |site_name|
+            site = connection.sites.select { |site| site.name == site_name }.first
+            fail "No site named #{site_name} on #{connection}" unless site
+            puts "Deleting #{site_name}/#{site.id} on #{connection.host}"
+            connection.delete_site(site.id)
+          end
+        end
+      end
+    end
+
     desc 'list', 'List sites'
     def list
       $connections.map do |connection|
         puts "#{connection.host}:"
-        connection.list_sites.map do |site_summary|
+        connection.sites.map do |site_summary|
           puts "\t#{site_summary.name}"
         end
+      end
+    end
+
+    desc 'scan', 'Scan sites/assets'
+    def scan(site_name, *assets)
+      $connections.map do |connection|
+        site = connection.sites.select { |site| site.name == site_name }.first
+        fail "No site named #{site_name} on #{connection}" unless site
+        puts "Scanning #{site_name}/#{site.id} on #{connection.host}"
+        connection.scan_site(site.id)
       end
     end
   end
